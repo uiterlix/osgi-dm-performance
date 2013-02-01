d8430e41ac85afa3085dc8d51619f2d622e0719b
Xander Uiterlinden <xander.uiterlinden@gmail.com>
2013-02-01 09:12:18 +0100

    Removed junk

--- projects/ds-performance-provider/src/org/example/provider/Globals.java
+++
@@ -1,7 +0,0 @@
-package org.example.provider;
-
-public class Globals {
-
-	static int SERVICE_COUNT = 100000;
-	static int TIMER_SPLIT = 50000;
-}
--- projects/ds-performance-provider/src/org/example/provider/ServiceFromFactory.java
+++
@@ -1,5 +0,0 @@
-package org.example.provider;
-
-public interface ServiceFromFactory {
-
-}
